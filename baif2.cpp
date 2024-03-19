aa513b32a8240085359668b49924079d467a1d51
buitiendat123 <151399731+buitiendat123@users.noreply.github.com>
2024-03-19 14:14:50 +0700

    Create bai2.cpp

---
+++ baif2.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+// Hàm kiểm tra xem một số có phải là số chính phương hay không
+int isPerfectSquare(int num) {
+    float fVar = sqrt((double)num);
+    int iVar = fVar;
+    return iVar == fVar;
+}
+
+// Hàm đếm và in ra các số chính phương nhỏ hơn n
+void countPerfectSquares(int n) {
+    printf("Cac so chinh phuong nho hon %d la:\n", n);
+    for (int i = 0; i < n; i++) {
+        if (isPerfectSquare(i)) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Nhap so nguyen duong n: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Vui long nhap so nguyen duong.\n");
+        return 1;
+    }
+
+    countPerfectSquares(n);
+
+    return 0;
+}
